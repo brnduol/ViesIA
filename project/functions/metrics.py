f86654cd3371aed624cfb7aebc6fc2fa364934ef
Matheus Fellet <matheusmfellet@gmail.com>
2025-06-26 17:08:38 -0300

    update: Adicionando docstrings

--- project/functions/metrics.py
+++ project/functions/metrics.py
@@ -4,6 +4,17 @@ import pandas as pd
 
 
 def false_positive_rate(df: pd.DataFrame) -> float:
+    '''
+    Computes the False Positive Rate in a pandas DataFrame.
+
+    Args:
+        df: Pandas DataFrame
+
+    Returns:
+        float: The false positive rate of the DataFrame
+
+    '''
+
     false_positives = df[(df['y_pred'] == 1) & (df['y_true'] == 0)]
     true_negatives = df[(df['y_pred'] == 0) & (df['y_true'] == 0)]
 
@@ -14,6 +25,20 @@ def statistical_parity_difference(
     df: pd.DataFrame,
     reference_group_idx: int,
 ) -> Dict[int, float]:
+    '''
+    Computes the Statistical Parity Difference between of the reference group with all others in the DataFrame.
+
+    The DataFrame must have columns ['y_true', 'y_pred', 'sensitive_attr'], with the 'sensitive_attr' columns needing to be encoded.
+
+    Args:
+        df: Pandas DataFrame.
+
+        reference_group_idx: index of reference group.
+
+    Returns:
+
+        dict: Dictionary with the SPD value for each of the groups. The group's SPD value is accessed with its encoded index as key.
+    '''
     reference_rate = df[df['sensitive_attr'] == reference_group_idx]['y_pred'].mean()
 
     spd_dict = {}
